77ebcd1a0185d2b75dc7da0dbd93d2035cc34203
shradhaamittal <126803919+shradhaamittal@users.noreply.github.com>
2026-02-20 13:19:56 +0000

    variables & datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail= "shradhamittal@gmail.com"
+var accountPassword=" 12345"
+accountCity="Delhi"
+let accountState
+/* in js, var is not preferred dur to functional scope and block scope. use let and const only */
+
+//accountId=2
+
+accountCity="blr"
+accountEmail="shradha@gmail.com"
+
+console.log(accountCity);
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; //to use the new version of node js
+
+//alert(3+3) used in browser, not node js
+
+console.log(3+3) //code readability should be high
+
+console.log("shradha")
+
+console.log(typeof "shradha")
+console.log(typeof null)
+console.log(typeof undefined)
